a929304d41cea8cc4eec8dc0470688f3fb25a99f
Millbee <jacobjacobatchison@gmail.com>
2025-07-25 00:31:41 -0500

    Entire backend

--- my-webapp/backend/crud.py
+++
@@ -1,9 +0,0 @@
-from database import get_db
-
-def insert_patient(patient):
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("INSERT INTO patients (name, dob, last_visit) VALUES (?, ?, ?)",
-                   (patient.name, patient.dob, patient.last_visit))
-    db.commit()
-    return {"status": "Patient added"}
